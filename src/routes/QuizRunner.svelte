fbe49604771385299e1f6f4014e3306443610a3d
Alessio Di Giacomo <>
2025-01-06 17:42:24 +0100

    Hide "switch layout" when on small screen devices

--- src/routes/QuizRunner.svelte
+++ src/routes/QuizRunner.svelte
@@ -11,6 +11,7 @@
     let showRecap = $state(false);
     let showAnswer = $state(false);
     let isHorizontalLayout = $state(false);
+    // svelte-ignore non_reactive_update
     let scrollPoint; // Used to store the scroll position when switching between questions
 
     let answeredQuestions = $derived(
@@ -87,7 +88,7 @@
         </button>
 
         <button
-            class="rounded-lg bg-gray-200 px-4 py-2 text-gray-800 transition-colors hover:bg-gray-300 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600"
+            class="hidden sm:inline rounded-lg bg-gray-200 px-4 py-2 text-gray-800 transition-colors hover:bg-gray-300 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600"
             onclick={() => (isHorizontalLayout = !isHorizontalLayout)}
         >
             {isHorizontalLayout ? '↕️ Layout Verticale' : '↔️ Layout Orizzontale'}
